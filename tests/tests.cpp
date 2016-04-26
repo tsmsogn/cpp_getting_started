06d4600b2b8794314d89e5add7e3fc6e83c153ff
tsmsogn <tsmsogn@gmail.com>
2016-04-26 14:46:02 +0900

    Initial commit

---
+++ tests/tests.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+TEST(sizeof, test1) {
+    char str1[10];
+    char str2[] = "Hello World!";
+
+    EXPECT_EQ(10, sizeof(str1));
+    EXPECT_EQ(13, sizeof(str2));
+}
+
+TEST(memcpy, test1) {
+    int a[] = {1, 2, 3};
+    int b[3];
+     
+    memcpy(b, a, sizeof(int) * 3);
+
+    EXPECT_EQ(1, b[0]);
+    EXPECT_EQ(2, b[1]);
+    EXPECT_EQ(3, b[2]);
+}
+
+TEST(std, swap) {
+    int a = 1;
+    int b = 2;
+
+    std::swap(a, b);
+
+    EXPECT_EQ(2, a);
+    EXPECT_EQ(1, b);
+}
