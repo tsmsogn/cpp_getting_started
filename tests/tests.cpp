88e2c3b140417071edb201d06b771ed7d19464ad
tsmsogn <tsmsogn@gmail.com>
2016-05-31 00:32:05 +0900

    Add string to int, and int to string tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -14,3 +14,29 @@ TEST(array, test1) {
     EXPECT_EQ(1, foo[0]);
     EXPECT_EQ(0, foo[1]);
 }
+
+TEST(cast, string_to_int) {
+    EXPECT_EQ(0, '0' - 48);
+    EXPECT_EQ(1, '1' - 48);
+    EXPECT_EQ(2, '2' - 48);
+    EXPECT_EQ(3, '3' - 48);
+    EXPECT_EQ(4, '4' - 48);
+    EXPECT_EQ(5, '5' - 48);
+    EXPECT_EQ(6, '6' - 48);
+    EXPECT_EQ(7, '7' - 48);
+    EXPECT_EQ(8, '8' - 48);
+    EXPECT_EQ(9, '9' - 48);
+}
+
+TEST(cast, int_to_string) {
+    EXPECT_EQ('0', 0 + 48);
+    EXPECT_EQ('1', 1 + 48);
+    EXPECT_EQ('2', 2 + 48);
+    EXPECT_EQ('3', 3 + 48);
+    EXPECT_EQ('4', 4 + 48);
+    EXPECT_EQ('5', 5 + 48);
+    EXPECT_EQ('6', 6 + 48);
+    EXPECT_EQ('7', 7 + 48);
+    EXPECT_EQ('8', 8 + 48);
+    EXPECT_EQ('9', 9 + 48);
+}
